0367b15ab274e38d43d36998a55d49c7e6cc8807
Matthew Smilansky <msmilansky@gmail.com>
2023-08-06 12:18:04 -0700

    Add notes to 46. Best Score

--- src/scenes/ScoreScene.js
+++ src/scenes/ScoreScene.js
@@ -7,8 +7,9 @@ class ScoreScene extends BaseScene {
 
 	create() {
 		super.create();
-
+		// Retrieve best score from local storage
 		const bestScore = localStorage.getItem('bestScore');
+		// Add best score text that displays either best score or 0 if there's no best score
 		this.add.text(...this.screenCenter, `Best Score: ${bestScore || 0}`, this.fontOptions).setOrigin(0.5);
 	}
 }
