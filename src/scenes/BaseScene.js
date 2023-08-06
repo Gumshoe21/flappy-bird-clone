09adcef1908e4b95192c3d4a25a66570e50004e8
Matthew Smilansky <msmilansky@gmail.com>
2023-08-06 11:25:14 -0700

    Add comments to 44. Setup menu events.

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -14,12 +14,15 @@ class BaseScene extends Phaser.Scene {
 		this.add.image(0, 0, 'sky').setOrigin(0);
 	}
 
-	createMenu(menu) {
+	createMenu(menu, setupMenuEvents) {
 		let lastMenuPositionY = 0;
 		menu.forEach(menuItem => {
 			const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
-			this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
+
+			// textGO = text game object
+			menuItem.textGO = this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
 			lastMenuPositionY += this.lineHeight;
+			setupMenuEvents(menuItem);
 		});
 	}
 }
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -14,7 +14,27 @@ class MenuScene extends BaseScene {
 	create() {
 		super.create();
 
-		this.createMenu(this.menu);
+		// The bind() method creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
+		// this binds the context to be MenuScene
+		this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+		// this.createMenu(this.menu, (menuItem) => this.setupMenuEvents(menuItem));
+	}
+
+	setupMenuEvents(menuItem) {
+		// retrieve the text game object from the passed-in menu item
+		const textGO = menuItem.textGO;
+		// set the text game object as interactive so that we can set up some events on the text
+		textGO.setInteractive();
+
+		// set a pointer over (hover) event that sets the style->fill of the text (yellow)
+		textGO.on('pointerover', () => {
+			textGO.setStyle({ fill: '#ff0' });
+		});
+
+		// set a pointer out (remove pointer) event that sets the style->fill of the text (white)
+		textGO.on('pointerout', () => {
+			textGO.setStyle({ fill: '#fff' });
+		});
 	}
 }
 
